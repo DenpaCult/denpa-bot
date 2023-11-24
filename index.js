c62d204323b03a1f61f35a41566e15b331e753a1
lyptea <randomly132@gmail.com>
2023-11-24 03:59:40 -0600

    wip

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ client.on('messageDelete', async message => {
   const deleteGuardData = getDeleteGuardData(guildId)
 
   // if in allowlist, repost message
-  if (Object.keys(deleteGuardData.users).includes(message.author.id)) {
+  if (deleteGuardData.users.includes(message.author.id)) {
     const mainEmbed = new Discord.EmbedBuilder()
       .setColor(0xecdca8) // golden color
       .setURL('https://example.com') // This is a hack to get more than one image embed
@@ -225,35 +225,38 @@ client.on('messageUpdate', async (oldMessage, newMessage) => {
   const deleteGuardData = getDeleteGuardData(guildId)
 
   // if in allowlist, repost message
-  if (Object.keys(deleteGuardData.users).includes(oldMessage.author.id)) {
-    const mainEmbed = new Discord.EmbedBuilder()
-      .setColor(0xecdca8) // golden color
-      .setURL('https://example.com') // This is a hack to get more than one image embed
-      .setAuthor({
-        name: `${oldMessage.author.tag}`,
-        iconURL: oldMessage.author.displayAvatarURL()
+  if (deleteGuardData.users.includes(oldMessage.author.id)) {
+    client.channels.fetch(deleteGuardData.channelId).then(channel => {
+      const mainEmbed = new Discord.EmbedBuilder()
+        .setColor(0xecdca8) // golden color
+        .setURL('https://example.com') // This is a hack to get more than one image embed
+        .setAuthor({
+          name: `${oldMessage.author.tag}`,
+          iconURL: oldMessage.author.displayAvatarURL()
+        })
+        .setTimestamp()
+        .setFooter({ text: `ID: ${oldMessage.id}` })
+
+      // value can not be "" or null (presumably can't be falsey)
+      // so make sure not to call addFields if there is no message
+      mainEmbed.addFields({ name: 'Old Message Content', value: oldMessage.content ?? '[Empty]' })
+      mainEmbed.addFields({ name: 'Link', value: oldMessage.url })
+
+      const hyperlinks = oldMessage.content.match(urlRegex) ?? []
+      const attachment = oldMessage.attachments.first()
+
+      mainEmbed.setImage(attachment?.url ?? hyperlinks.shift() ?? null)
+
+      const embeds = []
+      embeds.push(mainEmbed)
+
+      // https://www.reddit.com/r/discordapp/comments/raz4kl/finally_a_way_to_display_multiple_images_in_an/
+      hyperlinks.forEach(hyperlink => {
+        embeds.push(new Discord.EmbedBuilder().setURL('https://example.com').setImage(hyperlink))
       })
-      .setTimestamp()
-      .setFooter({ text: `ID: ${oldMessage.id}` })
 
-    // value can not be "" or null (presumably can't be falsey)
-    // so make sure not to call addFields if there is no message
-    mainEmbed.addFields({ name: 'Old Message Content', value: oldMessage.content ?? '[Empty]' })
-
-    const hyperlinks = oldMessage.content.match(urlRegex) ?? []
-    const attachment = oldMessage.attachments.first()
-
-    mainEmbed.setImage(attachment?.url ?? hyperlinks.shift() ?? null)
-
-    const embeds = []
-    embeds.push(mainEmbed)
-
-    // https://www.reddit.com/r/discordapp/comments/raz4kl/finally_a_way_to_display_multiple_images_in_an/
-    hyperlinks.forEach(hyperlink => {
-      embeds.push(new Discord.EmbedBuilder().setURL('https://example.com').setImage(hyperlink))
+      channel.send({ embeds })
     })
-
-    oldMessage.channel.send({ embeds })
   }
 })
 
