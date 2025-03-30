940c2b7858dd8fa32432edeeca537e7d5a92b822
yasharkind <yasharkind95@gmail.com>
2025-03-30 16:08:14 +0330

    removed some checkings to see if it fixes

--- index.js
+++ index.js
@@ -842,18 +842,9 @@ client.distube
 
 // + hoogmeh koko role change approved by thea (every 10 minuts)
 client.on(Discord.Events.GuildAvailable, async guild => {
-  let kokorole = null
-  let logchan = null
-  if (config.kokorole === '' || config.kokorole === undefined) {
-    if (guild.id === '856649672117583872') {
-      // temp fix until the config gets updated :pout:
-      kokorole = await guild.roles.fetch('856669801005711401')
-      logchan = await guild.channels.fetch('856758819936796692')
-      logchan.send('torom is online')
-    } else {
-      return
-    }
-  }
+  let kokorole = await guild.roles.fetch('856669801005711401')
+  const logchan = await guild.channels.fetch('856758819936796692')
+  logchan.send('torom is online')
 
   if (guild.id === config.guild_id) {
     if (kokorole === null) {
