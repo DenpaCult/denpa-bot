ce13ce786c31b566895a52e1434f23a4f647b806
kajongwe <kajongwe@fastmail.com>
2025-06-24 11:09:33 -0500

    fix: switch to roleId instead of name

--- index.js
+++ index.js
@@ -844,8 +844,8 @@ let hue = 0
 
 // + hoogmeh koko role change approved by thea (every 10 minuts)
 client.on(Discord.Events.GuildAvailable, async guild => {
-  const koko = guild.roles.cache.find(r => r.name === config.kokorole)
-  if (!koko) throw new Error(`failed to find koko role with name '${config.kokorole}'`)
+  const koko = guild.roles.cache.get(config.kokorole)
+  if (!koko) throw new Error(`failed to find koko role with ID '${config.kokorole}'`)
 
   const increment = 360 / 13 // cycle takes just over 2 hours, slight drift in colours over time
 
